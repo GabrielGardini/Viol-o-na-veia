df55c0f3bc03f962f757a11c1fa5bbc4e51e570b
Gabriel <gabrielgardini1@gmail.com>
2024-02-04 21:11:38 -0300

    tries to fix font bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box sx={{ bgcolor: "white" }}>
+      <Box sx={{ bgcolor: "white", fontFamily: "Poppins" }}>
         <Header />
         <Box
           sx={{
